0acbe3e21c0ee58b9decc44298158340da2520e4
Maks <maxsagadin@yahoo.com>
2021-11-15 17:46:30 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Feed/Walk the dogs',
+        day: 'Oct 21st at 8:00am',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Leetcode problems',
+        day: 'Oct 21st at 12:22pm',
+        reminder: true,
+    }
+  ])
+
+  const [showAddTask, setShowAddTask] = useState (false)
+
+  // Add Task
+  const addTask = (task) => {
+    // ID will always be 1 greater than the heighest ID or 1
+    const id = (tasks.length > 0) ? tasks[tasks.length-1].id + 1 : 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((tasks) => tasks.id !== id ))
+  }
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) => 
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask} />} 
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : (<h2>No Tasks</h2>)}
     </div>
-  );
+    
+  )
 }
 
 export default App;
+ 
